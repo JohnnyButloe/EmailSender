243f1cb3f4a6ad524c0d35c49a311a9c39dc86cd
John Butler <141792439+JohnnyButloe@users.noreply.github.com>
2024-03-03 11:38:01 -0500

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -5,8 +5,8 @@ from pathlib import Path # os.path
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'John Butler'
-email['to'] = 'jbutl003@gmail.com'
+email['from'] = 'Name'
+email['to'] = 'Name@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars!'
 
 email.set_content(html.substitute({'name': 'TinTin'}), 'html')
@@ -14,6 +14,6 @@ email.set_content(html.substitute({'name': 'TinTin'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('jbutl003@gmail.com', 'wstr njzi eewg nrwt')
+	smtp.login('Name@gmail.com', 'wstr njzi eewg nrwt')
 	smtp.send_message(email)
 	print('all good boss!')
